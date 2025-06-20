2eccf259faa285c5f01969efd93146d1e8dac033
Vaibhavr699 <78257023+Vaibhavr699@users.noreply.github.com>
2025-06-21 02:02:29 +0530

    Backend

--- utils/emailService.js
+++ utils/emailService.js
@@ -22,7 +22,7 @@ const sendEmail = async options => {
 
     // 2) Define the email options
     const mailOptions = {
-      from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+      from: `ToDo Flow <${process.env.FROM_EMAIL}>`,
       to: options.email,
       subject: options.subject,
       text: options.message,
